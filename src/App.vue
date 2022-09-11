1f4f905cb6b33da87451fc699d27c9c2f3c43a2b
IsabelSimon <simon.isabel17@gmail.com>
2022-09-11 16:08:59 -0500

    Segundo commit

--- src/App.vue
+++ src/App.vue
@@ -19,17 +19,15 @@
 </script>
 
 <template>
- <!-- <div class="alert alert-success text-center fixed-top " role="alert">
-  <h4 class="alert-heading">C1-S1</h4>
+ <div class="alert alert-success text-center fixed-top " role="alert">
   <p>Isabel Cristina Simón Morales</p>
   <hr>
   <p class="mb-0">10° "A" DGS</p>
-</div> -->
+</div>
 
 
- <!-- <div class="mtop"> -->
- <img src="./assets/logo.svg"  alt="logo" width="125" height="125">
-  <div class="container mtop">
+ <img src="./assets/logo.svg"  alt="logo" width="125" height="125" class="mtC">
+  <div class="container mtC">
     <h1> Hola desde VUE.js 3  </h1> <hr>
   <h2>
     Contador {{contador}}
@@ -40,24 +38,20 @@
   <button @click="decrementar">-1</button>
   <button @click="reset">Reset</button>
   </div>
- 
-
- <!-- </div> -->
   
 </template>
 
 <style scoped>
-
+  .mtC{
+    margin-top: 150px;
+  }
   h1{
   font-weight: bolder;
     /* color:red; */
   }
   .container{
   text-align: center;
-  }
-  .mbo{
-    margin-bottom: 155px;
-  }
+  } 
 
   small{
     color:#666;
